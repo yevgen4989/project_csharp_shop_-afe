8554cf0bc8d13c20c86f6123e00031cecbb04924
e.pisotckii <yevgenpisotsky@outlook.com>
2019-05-11 13:31:36 +0300

    Готовы все страницы и сделан popup для галереи в футтере

--- js/app.js
+++ js/app.js
@@ -43,4 +43,64 @@
 
 
     });
+
+
+    $(document).ready(function(){
+        $("#back-top").hide();
+        $(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 120) {
+                    $('#back-top').fadeIn();
+                }
+                else {
+                    $('#back-top').fadeOut();
+                }
+            });
+            $('#back-top a').click(function () {
+                $('body,html').animate({scrollTop: 0}, 800);
+                return false;
+            });
+        });
+    });
+
+    $(document).on('ready', function () {
+        var h = $('.info-about').height();
+        document.querySelector("#myGallery").style.height = h + "px";
+
+        var h_img = $('.img-reservation').innerHeight();
+        var h_form = $('.reservation-block').innerHeight();
+        var h_for_form_1 = (h_img - h_form) / 2;
+        document.querySelector("#reservation-block-id").style.padding = h_for_form_1 + "px" + " 7.5%";
+
+        $('.vertical-center').slick({
+            dots: true,
+            infinite: true,
+            arrows: false,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            centerMode: true,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 1500000,
+        });
+        $(".lazy").slick({
+            lazyLoad: 'ondemand',
+            infinite: true,
+            dots: false,
+            // autoplay: true,
+            // autoplaySpeed: 3000,
+            arrows: true,
+            fade: true,
+            speed: 350,
+            centerMode: true,
+            adaptiveHeight: true,
+
+        });
+
+    });
+
+
+
+
 })(jQuery);
