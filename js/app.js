ffc280e837d31ff32387bbca67ba0d2137cc27c1
e.pisotckii <yevgenpisotsky@outlook.com>
2019-06-20 01:47:22 +0300

    DONE PROJECT

--- js/app.js
+++ js/app.js
@@ -9,13 +9,23 @@ $(function () {
     "use strict";
 
 
+
+    $(window).scroll(function() {
+        if ($(".header-menu-logo").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("nav-scroll");
+        } else {
+            $(".navbar-fixed-top").removeClass("nav-scroll");
+        }
+    });
+
+
     $(function () {
         var H;
-        var menuSubstrate = $('.liner');
+        var menuSubstrate = $('.header-menu-logo');
         var linerHeight = menuSubstrate.outerHeight();
 
-        if ($("div").is(".track")) {
-            H = $(".track").outerHeight() - $(".header").outerHeight();
+        if ($("div").is(".header-menu-logo")) {
+            H = $(".header-menu-logo").outerHeight() / 2;
         } else {
             H = 0;
         }
@@ -27,16 +37,10 @@ $(function () {
         });
 
         function linerActivity() {
-            if ($(window).scrollTop() > H) {
-                $('.liner').addClass('liner_fixed');
-                $('.liner-fix').height(linerHeight);
-                menuSubstrate.css({
-                    'height': $('.liner').outerHeight()
-                });
-            } else if ($(window).scrollTop() < H) {
-                $('.liner-fix').height(0);
-                $('.liner').removeClass('liner_fixed');
-                menuSubstrate.css('height', 'auto');
+            if ($(window).scrollTop() > 1) {
+                $('.header-menu-logo').addClass('header-menu-fixed');
+            } else if ($(window).scrollTop() < 1) {
+                $('.header-menu-logo').removeClass('header-menu-fixed');
             }
         }
 
@@ -66,13 +70,44 @@ $(function () {
     });
 
     $(document).on('ready', function () {
-        var h = $('.info-about').height();
-        document.querySelector("#myGallery").style.height = h + "px";
 
-        var h_img = $('.img-reservation').innerHeight();
-        var h_form = $('.reservation-block').innerHeight();
-        var h_for_form_1 = (h_img - h_form) / 2;
-        document.querySelector("#reservation-block-id").style.padding = h_for_form_1 + "px" + " 7.5%";
+        $('.carouser-staff').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3500,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+
 
         $('.vertical-center').slick({
             dots: true,
@@ -84,14 +119,14 @@ $(function () {
             centerMode: true,
             adaptiveHeight: true,
             autoplay: true,
-            autoplaySpeed: 1500000,
+            autoplaySpeed: 3500,
         });
         $(".lazy").slick({
             lazyLoad: 'ondemand',
             infinite: true,
             dots: false,
-            // autoplay: true,
-            // autoplaySpeed: 3000,
+            autoplay: true,
+            autoplaySpeed: 3500,
             arrows: true,
             fade: true,
             speed: 350,
